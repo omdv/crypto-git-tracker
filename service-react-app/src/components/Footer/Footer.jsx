ea2effdea343a05312356d7b05759f5568b75d3a
Oleg Medvedev <omdv@fastmail.com>
2018-03-14 20:52:42 -0500

    Prior to react app ejecting

--- service-react-app/src/components/Footer/Footer.jsx
+++ service-react-app/src/components/Footer/Footer.jsx
@@ -4,8 +4,11 @@ import './Footer.css'
 export default () =>
   <footer className="Footer">
   	<div className="row">
+  		<p>
+	      <a href="https://github.com/omdv/crypto-git-tracker">Source Code ??</a>
+	    </p>
 	    <p>
-	      <a href="https://github.com/omdv">Source Code</a>
+	      <a href="https://github.com/omdv/crypto-git-tracker">Source Code</a>
 	    </p>
     </div>
   </footer>
